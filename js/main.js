b1d47a6055a4711f025fb2b62385978cb53eb532
Alex Story <astory11@yahoo.com>
2014-10-30 14:03:31 -0500

    debugged

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-var board = [[blackRook,blackKnight,blackBishop,blackQueen,blackKing,blackBishop,blackKnight,blackRook],[blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn],[whiteRook,whiteKnight,whiteBishop,whiteQueen,whiteKing,whiteBishop,whiteKnight,whiteRook]];
+var board = [[0,0,0,0,0,0,],[0,0,0,0,0,0,],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0,],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+//var board = [[blackRook,blackKnight,blackBishop,blackQueen,blackKing,blackBishop,blackKnight,blackRook],[blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn],[whiteRook,whiteKnight,whiteBishop,whiteQueen,whiteKing,whiteBishop,whiteKnight,whiteRook]];
 $(document).ready(function(){
   createTable(board);
 });
 
 function createTable(board){
-  $(table).innerHTML = '';
+  $('table').innerHTML = '';
   board.forEach(function(row){
   var $tr = $('<tr>');
   row.forEach(function(cell){
     var $td = $('<td>');
-    $tr.appendChild($td);
+    $tr.append($td);
   });
-  $(table).appendChild($td);
+  $('table').append($tr);
   });
 }
