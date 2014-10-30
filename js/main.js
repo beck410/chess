d4f71f774fecbf11bc3b0c10c2a9fa08e9c9c5d2
Alex Story <astory11@yahoo.com>
2014-10-30 12:20:45 -0500

    Added table creater

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-
 $(document).ready(function(){
   createTable(board);
 });
 
+function createTable(board){
+  $(table).innerHTML = '';
+  board.forEach(function(row){
+  var $tr = $(<'tr'>);
+  row.forEacf(cell){
+    var $td = $('<td>');
+    $tr.appendChild($td);
+  });
+  $(table).appendChild($td);
+  });
+}
