b99e14462c0e0242c5fd10e5357a9f90a59d50bc
beck410 <beck410@hotmail.com>
2014-10-31 15:37:35 -0500

    pulled down from github

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-  var x;
+var x;
   var y;
   var data = [];
 
@@ -74,29 +74,21 @@ function createTable(board){
 var greyRook = {
   image:"images/g-rook.png",
   move: function(){
-  },
-  showMove: function(){
   }
 };
 var greyKnight = {
   image:"images/g-knight.png",
   move: function(){
-  },
-  showMove: function(){
   }
 };
 var greyBishop = {
   image:"images/g-bishop.png",
   move: function(){
-  },
-  showMove: function(){
   }
 };
 var greyKing = {
   image:"images/g-king.png",
   move: function(){
-  },
-  showMove: function(){
   }
 };
 var greyQueen = {
@@ -105,7 +97,12 @@ var greyQueen = {
     //all squares left
     for(var i=y-i; i<=0; i--){
       if(board[x][i][0]){
-        board[x][i][1] = 1;
+        if(board[x][i][0].team === board[x][y][0].team){
+          return;
+        }
+        else{
+          board[x][i][1] = 1;
+        }
         return;
       } else{
         board[x][i][1] = 1;
@@ -113,37 +110,78 @@ var greyQueen = {
     }
     //all squares right
     for(var i=y+1; i<8; i++){
+      if(board[x][i][0]){
+        if(board[x][i][0].team === board[x][y][0].team){
+          return;
+        }
+        else{
+          board[x][i][1] = 1;
+        }
+        return;
+      } else{
+        board[x][i][1] = 1;
+      }
     }
 
     //all squares below
     for(var i=x+1; i<8; i++){
+      if(board[i][y][0]){
+        if(board[i][y][0].team === board[x][y][0].team){
+          return;
+        }
+        else{
+          board[i][y][1] = 1;
+        }
+        return;
+      } else{
+        board[i][y][1] = 1;
+      }
     }
 
     //all squares diag top-left
     for(var i=x, j=y; i<0 || j<=0; i--, j--){
+      if(board[x][i][0]){
+        board[x][i][1] = 1;
+        return;
+      } else{
+        board[x][i][1] = 1;
+      }
     }
 
     //all squares diag top-right
     for(var i=x, j=y; i<=0 || j<=0; i--, j++){
+      if(board[x][i][0]){
+        board[x][i][1] = 1;
+        return;
+      } else{
+        board[x][i][1] = 1;
+      }
     }
 
     //all squares diag bottom-right
     for(var i=x, j=y; i<=0 || j<=0; i++, j++){
+      if(board[x][i][0]){
+        board[x][i][1] = 1;
+        return;
+      } else{
+        board[x][i][1] = 1;
+      }
 
     }
     //all squares diag bottom-left
     for(var i=x, j=y; i<=0 || j<=0; i++, j--){
-
+      if(board[x][i][0]){
+        board[x][i][1] = 1;
+        return;
+      } else{
+      board[x][i][1] = 1;
+      }
     }
-  },
-  showMove: function(){
   }
 };
 var greyPawn = {
   image:"images/g-pawn.png",
   move: function(){
-  },
-  showMove: function(){
   }
 };
 
@@ -151,43 +189,31 @@ var greyPawn = {
 var orangePawn = {
   image: "images/o-pawn.png",
   move: function(){
-  },
-  showMove: function(){
-  },
+  }
 };
 var orangeRook = {
   image: "images/o-rook.png",
   move: function(){
-  },
-  showMove: function(){
-  },
+  }
 };
 var orangeKnight = {
   image: "images/o-knight.png",
   move: function(){
-  },
-  showMove: function(){
-  },
+  }
 };
 var orangeBishop = {
   image: "images/o-bishop.png",
   move: function(){
-  },
-  showMove: function(){
-  },
+  }
 };
 var orangeQueen= {
   image: "images/o-queen.png",
   move: function(){
   },
-  showMove: function(){
-  },
 };
 var orangeKing = {
   image: "images/o-king.png",
   move: function(){
-  },
-  showMove: function(){
   },
 };
 
